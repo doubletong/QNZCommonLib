5f9d3e3ff800f4280e10b58c040dfd79452ecf2f
tongjugang <ytjang@msn.com>
2019-09-15 20:19:15 +0800

    add readme

--- SIG.Infrastructure/Email/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SIG.Infrastructure.Email
-{
-    public interface IEmailService
-    {
-        void SendMail(string sender, string senderEmail, string mailTo, string mailcc, string subject, string body,
-           string smtpServer, string fromEmail, string displayName, string userName, string password, int port, bool enableSsl);
-    }
-}
